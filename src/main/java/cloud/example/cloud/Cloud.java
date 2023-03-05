df61244ee33a6194593265e579524d3147646a12
unknown <priyankadeshpancde113@gmail.com>
2023-03-05 08:27:09 +0530

    CodePush

--- src/main/java/cloud/example/cloud/Cloud.java
+++ src/main/java/cloud/example/cloud/Cloud.java
@@ -3,7 +3,7 @@ package cloud.example.cloud;
 import org.springframework.web.bind.annotation.GetMapping;
 
 public class Cloud {
-    @GetMapping("/Cloud")
+    @GetMapping("/Cloud devops")
     public String getData(){
         return"store the data";
 
