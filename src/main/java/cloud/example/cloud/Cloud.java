00301cb573cb8dde5f8fdfdf8a210cfa526a0f83
unknown <priyankadeshpancde113@gmail.com>
2023-03-05 09:06:00 +0530

    hello

--- src/main/java/cloud/example/cloud/Cloud.java
+++ src/main/java/cloud/example/cloud/Cloud.java
@@ -3,9 +3,10 @@ package cloud.example.cloud;
 import org.springframework.web.bind.annotation.GetMapping;
 
 public class Cloud {
-    @GetMapping("/Cloud devops.")
+    @GetMapping("/Clouddevops")
     public String getData(){
         return"store the data";
 
     }
+
 }
