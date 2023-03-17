3ad4b52ac811750b861b47998357a3993a7fb456
unknown <priyankadeshpancde113@gmail.com>
2023-03-18 00:22:46 +0530

    code change

--- src/main/java/cloud/example/cloud/CloudDemo.java
+++ src/main/java/cloud/example/cloud/CloudDemo.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CloudDemo {
     @GetMapping("/mycloud")
     public String getData(){
-        return "store the data";
+        return "store the data on cloud.....";
 
     }
 }
